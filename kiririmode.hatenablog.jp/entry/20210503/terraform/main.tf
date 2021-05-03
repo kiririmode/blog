87b7bb8126bb917db7b285cc777f7a774f4e4f14
kiririmode <kiririmode@gmail.com>
2021-05-03 16:27:35 +0900

    add: AzureでTerraform Backendを構成する

---
+++ kiririmode.hatenablog.jp/entry/20210503/terraform/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = "0.15.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.56.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "kiririmodestatestorage"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "japaneast"
+}
