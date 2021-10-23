2baa867eddd9089811cd52014445029add401866
kiririmode <kiririmode@gmail.com>
2021-10-24 01:20:57 +0900

    add: GitLab CI/CDで生成したファイルをGeneric Packages Repositoryに配置する

--- scripts/check.sh
+++ scripts/check.sh
@@ -6,7 +6,9 @@ TIMESTAMP_FILE="last_publish_time"
 function check {
     local entries=$(git log --name-only --pretty="tformat:" --since $(cat $TIMESTAMP_FILE) -- kiririmode.hatenablog.jp/entry/)
 
-    npx textlint $entries
+    if [[ -n $entries ]]; then
+        npx textlint $entries
+    fi
 }
 
 check
